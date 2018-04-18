ad02016cd237faa8e9a7474f28cb4e30f92946d5
Peter Bowyer <peter@mapledesign.co.uk>
2018-04-18 17:36:21 +0100

    New event

--- core/model/modx/modparser.class.php
+++ core/model/modx/modparser.class.php
@@ -264,17 +264,13 @@ class modParser {
                 $processed+= $this->processElementTags($parentTag, $content, $processUncacheable, $removeUnprocessed, $prefix, $suffix, $tokens, $depth);
             }
         }
-        /** @var modTemplateParser $parser */
-        foreach ($this->modx->getTemplateParsers() as $parser) {
-            $parser->setParentTag($parentTag);
-            $parser->setProcessUncacheable($processUncacheable);
-            $parser->setRemoveUnprocessed($removeUnprocessed);
-            $parser->setPrefix($prefix);
-            $parser->setSuffix($suffix);
-            $parser->setTokens($tokens);
-            $parser->setDepth($depth);
-            $content = $parser->parse($content);
-        }
+
+        $ret = $this->modx->invokeEvent('OnAfterParseDocument', array(
+            'parentTag' => $parentTag,
+            'content' => &$content,
+            'placeholders' => $this->modx->placeholders,
+            'depth' => $depth,
+        ));
 
         $this->_removingUnprocessed = $_removingUnprocessed;
         $this->_processingUncacheable = $_processingUncacheable;
